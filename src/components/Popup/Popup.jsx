e45a72ba0cd1ab7990355678a2f5cacbe1d958f3
Kseniia Tikhonova <tikhonovakb@gmail.com>
2024-02-22 19:24:09 +0100

    hotfix: fixing popup clousing

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import styles from './Popup.module.css';
+import { CiCircleRemove } from 'react-icons/ci';
 
 const Popup = ({closePopup}) => {
   const handleOnClose = (e) => {
-    if (e === 'parent') closePopup();
+    if (e.target.id === 'parent') closePopup();
   };
 
   return (
-    <div className={styles.parent} id="parent" onClick={handleOnClose}>
+    <div className={styles.parent} id="parent" onClick={handleOnClose}>    
       <div className={styles.container}>
+      <CiCircleRemove className={styles.closeButton} />
+      
         <p3 className={styles.title}>Subscribe to our events</p3>
         <form className={styles.form}>
           <input className={styles.input} placeholder="enter your email" />
-          <button className={styles.button}>Subscribe now</button>
+          <button className={styles.confirmButton}>Subscribe now</button>
         </form>
       </div>
     </div>
