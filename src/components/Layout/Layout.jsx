2278f04bfb379e1b798eaebeee86bd50b53a777c
Hassen Ahmed <hassan.creative@gmail.com>
2024-02-26 12:53:54 +0100

    adding images

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../../components/Footer";
 
 const Layout = () => {
   return (
@@ -6,6 +7,7 @@ const Layout = () => {
       {/* We have have our Header component  */}
       <Outlet />
       {/* We have have our Footer component  */}
+      <Footer />
     </>
   );
 };
