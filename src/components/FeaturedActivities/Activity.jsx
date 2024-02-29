3c99c261c07596a8775092bdb95cb0684ee96c1c
Hassen Ahmed <hassan.creative@gmail.com>
2024-02-29 21:07:14 +0100

    some fixation

--- src/components/FeaturedActivities/Activity.jsx
+++ src/components/FeaturedActivities/Activity.jsx
@@ -3,14 +3,26 @@ import classes from "./Activity.module.css";
 import { BASENAME } from "../../constant";
 
 const Activity = ({ activity, posterView = false }) => {
-  const { title, category, poster, thumbnail } = activity;
+  const { title, category, poster, thumbnail, thumbnail_mob } = activity;
   return (
     <div className={classes.activity}>
-      <img
+      {/* <img
         className={classes.activity__image}
         src={`${BASENAME}${thumbnail}`}
         alt={title}
-      ></img>
+      ></img> */}
+      <picture className={classes.activity__image}>
+        <source
+          media="(max-width: 400px)"
+          srcSet={`${BASENAME}${thumbnail_mob}`}
+        />
+        <source media="(min-width: 400px)" srcSet={`${BASENAME}${thumbnail}`} />
+        <img
+          src={`${BASENAME}${thumbnail_mob}`}
+          alt={title}
+          style={{ width: "auto" }}
+        />
+      </picture>
       <div className={classes.activity__mask}>
         <p className={classes.activity__title}>{title}</p>
         <p className={classes.activity__category}>{category}</p>
