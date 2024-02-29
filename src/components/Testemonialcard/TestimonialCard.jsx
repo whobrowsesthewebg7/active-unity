1dfc6f8648a7dca3b989630cfff383f8d5ad878f
Hassen Ahmed <hassan.creative@gmail.com>
2024-02-29 21:08:46 +0100

    fixing testimonial

--- src/components/Testemonialcard/TestimonialCard.jsx
+++ src/components/Testemonialcard/TestimonialCard.jsx
@@ -3,35 +3,19 @@ import { BASENAME } from "../../constant";
 
 const TestimonialCard = ({ name, image, date, statement }) => {
   return (
-    <>
-      <figure className={styles.testimonial__card} role="blockquote">
-        <img
-          src={`${BASENAME}${image}`}
-          className={styles.testimonial__picture}
-          alt={name}
-          role="img"
-        />
-        <figcaption className={styles.testimonial__who}>
-          <cite className={styles.testimonial__name}>{name}</cite>
-          <time className={styles.testimonial__date}>{date}</time>
-        </figcaption>
-        <p className={styles.testimonial__statement}>{statement}</p>
-      </figure>
-
-      {/* <figure className={styles.testimonial__card} role="blockquote">
-<img
-  src={`${BASENAME}${image}`}
-  className={styles.testimonial__picture}
-  alt={name}
-  role="img"
-/>
-<figcaption className={styles.testimonial__who}>
-  <cite className={styles.testimonial__name}>{name}</cite>
-  <time className={styles.testimonial__date}>{date}</time>
-</figcaption>
-<p className={styles.testimonial__statement}>{statement}</p>
-</figure> */}
-    </>
+    <figure className={styles.testimonial__card} role="blockquote">
+      <img
+        src={`${BASENAME}${image}`}
+        className={styles.testimonial__picture}
+        alt={name}
+        role="img"
+      />
+      <figcaption className={styles.testimonial__who}>
+        <cite className={styles.testimonial__name}>{name}</cite>
+        <time className={styles.testimonial__date}>{date}</time>
+      </figcaption>
+      <p className={styles.testimonial__statement}>{statement}</p>
+    </figure>
   );
 };
 
